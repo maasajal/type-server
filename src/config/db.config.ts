8d536211b2671f64e67ae47a0e2da128606d7210
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2024-10-06 20:24:30 +0300

    Bug: trying to fix deployment bug

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,13 +1,29 @@
+import { log } from "console";
 import "dotenv/config";
-import mongoose from "mongoose";
+import mongoose, { connect, connections } from "mongoose";
+import { env } from "process";
+let isConnected: boolean = false;
+
 export const ConnectDB = async () => {
+  if (isConnected) {
+    console.log("Using existing database connection");
+    return;
+  }
+
   try {
     const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jd9hrzt.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`;
-    await mongoose.connect(uri);
+    // await mongoose.connect(uri);
+
+    const db = await mongoose.connect(uri!);
+
+    isConnected = !!db.connections[0].readyState;
+    console.log("Connected to MongoDB");
+
     // await mongoose.connect(`${process.env.DB_URI}`);
-    console.log(`the DB is connected with ${mongoose.connection.host}`);
+    // console.log(`the DB is connected with ${mongoose.connection.host}`);
   } catch (error) {
-    console.error("Error connecting to the database", error);
-    mongoose.disconnect();
+    console.error("Failed to connect to MongoDB:", error);
+    throw new Error("Database connection failed");
+    // mongoose.disconnect();
   }
 };
--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from "express";
 import User from "../models/userModel";
 import generateToken from "../utils/generateToken";
+import { ConnectDB } from "../config/db.config";
 
 export const registerUser = async (req: Request, res: Response) => {
+  await ConnectDB();
   try {
     const userData = req.body;
     const { email } = userData;
@@ -15,10 +17,12 @@ export const registerUser = async (req: Request, res: Response) => {
     if (addUser) {
       const token = generateToken(email);
       res.status(200).send({ message: token });
+    } else {
+      res.status(400).send({ message: "Invalid user data" });
     }
   } catch (error) {
-    console.log(error);
-    res.status(400).send({ message: "Invalid user data" });
+    console.error("Server error:", error);
+    res.status(500).send({ message: "Internal Server Error" });
   }
 };
 
